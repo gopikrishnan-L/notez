58cc74279492419f5dff2651fa436a49122148cc
gopikrishnan-L <125388612+gopikrishnan-L@users.noreply.github.com>
2024-10-12 22:08:58 +0530

    style: blog card component design change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // "use client";
 
 import { getAllBlogs } from "@/db/blogs";
+import { getUserAsCreator } from "@/db/users";
 
 // import { useEffect, useState } from "react";
 
@@ -16,6 +17,8 @@ export default async function Home() {
             key={blog.id}
             title={blog.title}
             description={blog.description}
+            creatorId={blog.creatorId}
+            date={blog.createdAt}
           />
         ))}
       </div>
@@ -61,23 +64,51 @@ function HighLights() {
   );
 }
 
-function Post({ title, description }: { title: string; description: string }) {
+async function Post({
+  title,
+  description,
+  creatorId,
+  date,
+}: {
+  title: string;
+  description: string;
+  creatorId: string;
+  date: Date;
+}) {
+  const creator = await getUserAsCreator(creatorId);
   return (
-    <div className="flex flex-col group relative w-full h-[22rem] rounded-lg border-[1px] p-2">
-      <div className="h-[60%] w-full mx-auto overflow-clip rounded-lg">
+    <div className="flex flex-col group relative w-full h-[25rem] rounded-lg border-[1px] p-2">
+      <div className="h-[50%] w-full mx-auto overflow-clip rounded-lg">
         <img
           src="https://images.pexels.com/photos/28494944/pexels-photo-28494944/free-photo-of-creative-portrait-with-mirror-reflection-in-berlin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="blog-pic"
           className="object-scale-down"
         />
       </div>
-      <section className=" mt-2 mb-2  text-xs font-medium p-1 bg-violet-100 text-violet-600 w-fit rounded-md">
-        Tag/Genre
-      </section>
-      <p className="text-lg font-semibold mb-2">{title}</p>
-      <p className="text-sm overflow-hidden overflow-ellipsis h-[1rem]">
-        {description}
-      </p>
+      <div className="flex flex-col gap-2 px-2 h-full">
+        <section className=" mt-2 mb-2 text-xs font-medium p-1 bg-violet-100 text-violet-600 w-fit rounded-md">
+          Tag/Genre
+        </section>
+        <section className="max-h-[12ch] overflow-hidden line-clamp-3">
+          <p className="text-lg font-semibold mb-2 overflow-hidden line-clamp-2">
+            {title}
+          </p>
+          <p className="text-sm">{description}</p>
+        </section>
+        <div className="mt-auto pt-2 flex gap-2 items-center text-xs">
+          <div className="w-8 aspect-square rounded-full overflow-hidden bg-gray-700">
+            <img src={creator?.image!} alt={creator?.name!} />
+          </div>
+          <span>{creator?.name}</span>
+          <span className="ml-auto">
+            {date.getDate() +
+              "th " +
+              date.toLocaleString("default", { month: "long" }) +
+              ", " +
+              date.getFullYear()}
+          </span>
+        </div>
+      </div>
       {/* <div className="w-full absolute bottom-0 p-4 bg-[linear-gradient(hsl(0_0%_0%/0),hsl(20_0%_0%/0.3)_20%,hsl(0_0%_0%/1))]">
         <div className="max-h-[10ch] text-lg text-white mb-2">{title}</div>
         <div className="max-h-0  text-sm group-hover:max-h-[14ch] text-white opacity-0 group-hover:opacity-100 transition-all duration-500 ease-[cubic-bezier(1,0,0,.5)]  delay-100">
