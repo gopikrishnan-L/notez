555caae9e006c30677da19d7b9ee59ccbd62268a
gopikrishnan-L <125388612+gopikrishnan-L@users.noreply.github.com>
2024-10-12 21:12:22 +0530

    style: home page layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <div className="grid grid-cols-[3fr_1fr] max-w-[75rem] mx-auto gap-4 max-xl:max-w-[60rem] max-lg:max-w-[40rem] max-lg:grid-cols-1">
       <HighLights />
       <div className="w-full h-full text-lg font-semibold">Editors' Picks</div>
-      <div className="grid grid-cols-3 gap-4 flex-wrap max-w-[50rem] max-lg:w-full lg:row-start-2">
+      <div className="grid grid-cols-3 gap-2 max-lg:w-full lg:row-start-2">
         {blogPosts.map((blog, i) => (
           <Post
             key={blog.id}
@@ -45,7 +45,7 @@ function HighLights() {
   return (
     <div className="lg:col-span-2 mb-8">
       {/* <p className="text-3xl font-bold mb-2">Highlights of The Week</p> */}
-      <section className="grid h-[35rem] grid-cols-[3fr_1fr] gap-2 mb-4 transition-all">
+      <section className="grid h-[38rem] grid-cols-[3fr_1fr] gap-4 mb-4 transition-all">
         {highlightCards.map((card, i) => (
           <div
             key={i}
@@ -63,19 +63,27 @@ function HighLights() {
 
 function Post({ title, description }: { title: string; description: string }) {
   return (
-    <div className="group relative w-full h-[22rem] rounded-lg overflow-clip ">
-      <img
-        src="https://images.pexels.com/photos/28494944/pexels-photo-28494944/free-photo-of-creative-portrait-with-mirror-reflection-in-berlin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="blog-pic"
-        className="object-contain"
-      />
-      <div className="w-full absolute bottom-0 p-4 bg-[linear-gradient(hsl(0_0%_0%/0),hsl(20_0%_0%/0.3)_20%,hsl(0_0%_0%/1))]">
-        {/* //translate-y-[50%] transition-all group-hover:translate-y-0 */}
+    <div className="flex flex-col group relative w-full h-[22rem] rounded-lg border-[1px] p-2">
+      <div className="h-[60%] w-full mx-auto overflow-clip rounded-lg">
+        <img
+          src="https://images.pexels.com/photos/28494944/pexels-photo-28494944/free-photo-of-creative-portrait-with-mirror-reflection-in-berlin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="blog-pic"
+          className="object-scale-down"
+        />
+      </div>
+      <section className=" mt-2 mb-2  text-xs font-medium p-1 bg-violet-100 text-violet-600 w-fit rounded-md">
+        Tag/Genre
+      </section>
+      <p className="text-lg font-semibold mb-2">{title}</p>
+      <p className="text-sm overflow-hidden overflow-ellipsis h-[1rem]">
+        {description}
+      </p>
+      {/* <div className="w-full absolute bottom-0 p-4 bg-[linear-gradient(hsl(0_0%_0%/0),hsl(20_0%_0%/0.3)_20%,hsl(0_0%_0%/1))]">
         <div className="max-h-[10ch] text-lg text-white mb-2">{title}</div>
         <div className="max-h-0  text-sm group-hover:max-h-[14ch] text-white opacity-0 group-hover:opacity-100 transition-all duration-500 ease-[cubic-bezier(1,0,0,.5)]  delay-100">
           {description}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
